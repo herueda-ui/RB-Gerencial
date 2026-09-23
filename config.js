d04d29f64f2366fe570e6206f5a2d2e4ccc49157
herueda-ui <herueda@gmail.com>
2026-09-23 09:26:47 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.RB_CLOUD = {
+  supabaseUrl: "https://zfdohijwfmzwgjcmtroe.supabase.co",
+  publishableKey: "sb_publishable_-Tg-wBGn9SYlUxt3pN8EyQ_lSquYKGN",
+  branchCode: "RB-MANIZALES-01",
+  refreshSeconds: 15
+};
